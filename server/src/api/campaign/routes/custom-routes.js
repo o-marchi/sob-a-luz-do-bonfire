6396afb330e216292987366f97b66e1dd2bf1da1
Onelio de Marchi Junior <oneliojr@gmail.com>
2025-07-22 01:04:39 -0300

    Create custom-routes.js

---
+++ server/src/api/campaign/routes/custom-routes.js
@@ -0,0 +1,23 @@
+﻿
+module.exports = {
+  routes: [
+    {
+      method: 'GET',
+      path: '/current-campaign',
+      handler: 'campaign.getCurrentCampaign',
+      config: {
+        auth: false
+      },
+    },
+    {
+      method: 'POST',
+      path: '/update-player-game-information',
+      handler: 'campaign.updatePlayerGameInformation',
+      config: {
+        auth: {
+          required: true,
+        }
+      },
+    },
+  ],
+};
