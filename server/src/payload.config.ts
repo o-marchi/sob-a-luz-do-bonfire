2d1d04aa1f8d7bcd4dcfa9bc1ad8224483c9f6eb
Onelio de Marchi Junior <oneliojr@gmail.com>
2025-07-24 07:41:21 -0300

    Boostrap for deploy

--- server/src/payload.config.ts
+++ server/src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { postgresAdapter } from '@payloadcms/db-postgres'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -48,11 +49,18 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URI || '',
-    },
-  }),
+  db:
+    (process.env.DATABASE || 'postgres') === 'postgres'
+      ? postgresAdapter({
+          pool: {
+            connectionString: process.env.DATABASE_URI || '',
+          },
+        })
+      : sqliteAdapter({
+          client: {
+            url: process.env.DATABASE_URI || '',
+          },
+        }),
   sharp,
   plugins: [
     payloadCloudPlugin(),
