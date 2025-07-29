9641e78e6bbf506c4787b8990fd46cefca9b1eb1
Onelio de Marchi Junior <oneliojr@gmail.com>
2025-07-28 21:27:14 -0300

    Add prefix column to media table
    Introduces a new 'prefix' column with default 'sobaluz' to the 'media' table via migration. Updates migration index to include the new migration.

--- server/src/migrations/index.ts
+++ server/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250728_210706 from './20250728_210706';
 import * as migration_20250728_212505 from './20250728_212505';
+import * as migration_20250729_002644 from './20250729_002644';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250728_212505.up,
     down: migration_20250728_212505.down,
-    name: '20250728_212505'
+    name: '20250728_212505',
+  },
+  {
+    up: migration_20250729_002644.up,
+    down: migration_20250729_002644.down,
+    name: '20250729_002644'
   },
 ];
